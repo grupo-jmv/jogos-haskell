f85d07221e11d1929c53888b7c9363a99d49fe87
Jessica, Monica e Vinicius <jeferreira.ti@gmail.com>
2020-12-06 19:54:31 -0300

    Tela de Jogos

---
+++ src/Handler/Jogos.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Jogos where
+import Import
+import Text.Lucius
+
+getJogosR :: Handler Html
+getJogosR = do
+    jogos <- runDB $ selectList [] [Asc JogoNome]
+    defaultLayout $ do
+        sess <- lookupSession "_NOME"
+        addStylesheet (StaticR css_bootstrap_css)
+        toWidgetHead $(luciusFile "templates/jogos.lucius")
+        $(whamletFile "templates/jogos.hamlet")
