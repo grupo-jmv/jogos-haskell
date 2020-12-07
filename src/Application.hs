f9cd5547bc2a5f2ecfa076b552a6994035223a89
Jessica, Monica e Vinicius <jeferreira.ti@gmail.com>
2020-12-06 21:27:21 -0300

    Importação de tela de editar jogo

--- src/Application.hs
+++ src/Application.hs
@@ -47,6 +47,7 @@ import Handler.DesenvolvedoraEditar
 import Handler.DesenvolvedoraRemover
 import Handler.Jogo
 import Handler.Jogos
+import Handler.JogoEditar
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
