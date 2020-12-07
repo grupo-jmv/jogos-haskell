b18dce049121fc075f479f9623ba47ed05c2f892
Jessica, Monica e Vinicius <jeferreira.ti@gmail.com>
2020-12-06 21:25:52 -0300

    Tela de edição de jogo

---
+++ src/Handler/JogoEditar.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.JogoEditar where
+import Import
+import Database.Persist.Postgresql
+
+getJogoEditarR :: JogoId -> Handler Html
+getJogoEditarR pid = do
+    err <- runDB $ get404 pid
+    formEditarJogo (JogoEditarR pid) (Just err)
+
+formEditarJogo :: Route App -> Maybe Jogo -> Handler Html
+formEditarJogo rt dado = do
+    (widget, _) <- generateFormPost (formJogo dado)
+    defaultLayout $ do 
+        $(whamletFile "templates/jogoeditar.hamlet")
+
+formJogo :: Maybe Jogo -> Form Jogo
+formJogo jogo = renderBootstrap $ Jogo
+   <$> areq textField "Nome: " (fmap jogoNome jogo)
+   <*> areq dayField "Lançamento: " (fmap jogoLancamento jogo)
+   <*> areq (selectField desenvolvedoras) "Desenvolvedora: " (jogoDesenvolvedora <$> jogo)
+
+desenvolvedoras = do
+    rows <- runDB $ selectList [] [Asc DesenvolvedoraNome]
+    optionsPairs $
+        map (\r -> (desenvolvedoraNome $ entityVal r, entityKey r)) rows
+
+postJogoEditarR :: JogoId -> Handler Html
+postJogoEditarR pid = do
+    ((res, _), _) <- runFormPost (formJogo Nothing)
+    case res of
+        FormSuccess desenv -> do
+            _ <- runDB (replace pid desenv)
+            redirect JogosR
+        _ -> redirect HomeR
